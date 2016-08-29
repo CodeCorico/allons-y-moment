90c92450bcc7f7c652cbf63fd9375830460077a3
Xavier Boubert <xavier@boubert.fr>
2016-08-29 16:29:58 +0200

    feat(moment): create the feature

---
+++ features/moment/moment-gulpfile.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = function($gulp) {
+
+  $gulp.task('moment', function(done) {
+
+    $gulp.src([
+      'node_modules/moment/moment.js',
+      'node_modules/moment/min/moment.min.js',
+      'node_modules/moment/min/moment-with-locales.js',
+      'node_modules/moment/min/moment-with-locales.min.js'
+    ])
+      .pipe($gulp.dest('public/vendor'))
+      .on('end', done);
+  });
+
+  return 'moment';
+};
