71984831cc1e41b27f9a282756131c6568506bbb
Xavier Boubert <xavier@boubert.fr>
2016-10-19 11:56:21 +0200

    fix(moment): add relative path to $gulp.src()

--- features/moment/moment-gulpfile.js
+++ features/moment/moment-gulpfile.js
@@ -5,10 +5,10 @@ module.exports = function($gulp) {
   $gulp.task('moment', function(done) {
 
     $gulp.src([
-      'node_modules/moment/moment.js',
-      'node_modules/moment/min/moment.min.js',
-      'node_modules/moment/min/moment-with-locales.js',
-      'node_modules/moment/min/moment-with-locales.min.js'
+      './node_modules/moment/moment.js',
+      './node_modules/moment/min/moment.min.js',
+      './node_modules/moment/min/moment-with-locales.js',
+      './node_modules/moment/min/moment-with-locales.min.js'
     ])
       .pipe($gulp.dist('vendor'))
       .on('end', done);
